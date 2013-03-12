4082bc5c24a3fd1dcda5b288ace665b6bb9f43c1
Antoine Albertelli <antoine.albertelli@gmail.com>
2013-03-12 11:18:09 +0100

    Added a command to test PWMs for connecting the arms.

--- commands_cs.c
+++ commands_cs.c
@@ -2,8 +2,10 @@
 #include <parse_num.h>
 #include <stdio.h>
 #include <string.h>
+#include <cvra_dc.h>
 
 #include "cvra_cs.h"
+#include "adresses.h"
 
 
 /* Gets robot position. */
@@ -65,21 +67,27 @@ parse_inst_t cmd_position_set = {
 struct cmd_pwm_result {
     fixed_string_t arg0; ///< "pwm"
     fixed_string_t pwm_name;
+    int16_t channel;
     int16_t value;
 };
 
 static void cmd_pwm_parsed(void *r, __attribute__((unused)) void *data) {
+    int32_t *base;
     struct cmd_pwm_result *results = (struct cmd_pwm_result *)r;
+    if(!strcmp(results->pwm_name, "arm"))
+        base = ARMSMOTORCONTROLLER_BASE;
+    else
+        base = HEXMOTORCONTROLLER_BASE;
 
-    /* TODO Check with Rouven how this is supposed to work. */
-    printf("Setting PWM %s to %d", results->pwm_name, results->value);
+    cvra_dc_set_pwm(base, results->channel, results->value);
 }
 
 char str_pwm_arg0[] = "pwm";
 parse_token_string_t cmd_pwm_arg0 = TOKEN_STRING_INITIALIZER(struct cmd_pwm_result, arg0, str_pwm_arg0);
-char str_pwm_pwm_name[] = "brushless1#brushless2"; /*XXX Add other PWMs. */
+char str_pwm_pwm_name[] = "arm#base";
 parse_token_string_t cmd_pwm_pwm_name = TOKEN_STRING_INITIALIZER(struct cmd_pwm_result, pwm_name, str_pwm_pwm_name);
 
+parse_token_num_t cmd_pwm_channel = TOKEN_NUM_INITIALIZER(struct cmd_pwm_result, channel, INT16); 
 parse_token_num_t cmd_pwm_value = TOKEN_NUM_INITIALIZER(struct cmd_pwm_result, value, INT16); 
 char str_pwm_help[] = "Set PWM value.";
 
@@ -90,6 +98,7 @@ parse_inst_t cmd_pwm = {
     .tokens = {
         (void *)&cmd_pwm_arg0,
         (void *)&cmd_pwm_pwm_name,
+        (void *)&cmd_pwm_channel,
         (void *)&cmd_pwm_value,
         NULL,
     },
