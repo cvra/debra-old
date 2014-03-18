36e32453037f28fda58a5bc37b394ee38f44d689
Antoine Albertelli <antoinea101@gmail.com>
2014-03-18 10:34:18 +0100

    removed arm_linear_motion stuff.

--- arm_interpolators.c
+++ arm_interpolators.c
@@ -9,43 +9,6 @@ void arm_trajectory_init(arm_trajectory_t *traj) {
 }
 
 
-void arm_interpolator_linear_motion(arm_trajectory_t *traj, const float start[3], const float end[3],
-                                    arm_coordinate_t system, const float duration) {
-
-    int32_t time = uptime_get();
-
-    if(traj == NULL)
-        return;
-
-
-    traj->frame_count = 2;
-    traj->frames = malloc(2*sizeof(arm_keyframe_t));
-
-
-    traj->frames[0].coordinate_type = COORDINATE_ARM;
-    traj->frames[0].position[0] = start[0];
-    traj->frames[0].position[1] = start[1];
-    traj->frames[0].position[2] = start[2];
-
-    traj->frames[0].date = time;
-
-    traj->frames[0].length[0] = 135;
-    traj->frames[0].length[1] = 100;
-
-
-
-    traj->frames[1].coordinate_type = system;
-    traj->frames[1].position[0] = end[0];
-    traj->frames[1].position[1] = end[1];
-    traj->frames[1].position[2] = end[2];
-
-    traj->frames[1].date = time+(int)(duration * 1e6);
-
-
-    traj->frames[1].length[0] = 135;
-    traj->frames[1].length[1] = 100;
-}
-
 void arm_interpolator_append_point(arm_trajectory_t *traj, const float x, const float y, const float z,
                                    arm_coordinate_t system, const float duration)
 {
