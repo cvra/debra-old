6ed7569f968d0c1173cb8fc09f888fa138d01f75
Antoine Albertelli <antoinea101@gmail.com>
2014-03-18 21:56:47 +0100

    Split tests into several files.

---
+++ tests/arm_choose_elbow_pos.cpp
@@ -0,0 +1,98 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C" {
+#include "../arm_cinematics.h"
+}
+
+TEST_GROUP(ChooseElbowPositionTestGroup)
+{
+};
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorIdentity)
+{
+    shoulder_mode_t m = SHOULDER_FRONT;
+    float angle = 1;
+    CHECK_EQUAL(m, mode_for_orientation(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorInvert)
+{
+    shoulder_mode_t m = SHOULDER_FRONT;
+    float angle = -1;
+    CHECK_EQUAL(SHOULDER_BACK, mode_for_orientation(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ModeMirrorInvertBack)
+{
+    shoulder_mode_t m = SHOULDER_BACK;
+    float angle = -1;
+    CHECK_EQUAL(SHOULDER_FRONT, mode_for_orientation(m, angle));
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowSolutionOutsideRobot)
+{
+    // First test case : target_x < 0 ('inside' the robot)
+    point_t elbow1 = {.x=10, .y=10};
+    point_t elbow2 = {.x=-10, .y=10};
+    point_t target = {.x=-10, .y=20};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow1.x, chosen.x);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowSolutionOutsideRobotBis)
+{
+    // First test case : target_x < 0 ('inside' the robot)
+    point_t elbow1 = {.x=-10, .y=10};
+    point_t elbow2 = {.x=10, .y=10};
+    point_t target = {.x=-10, .y=20};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow2.x, chosen.x);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowBackward)
+{
+    point_t elbow1 = {.x=10, .y=10};
+    point_t elbow2 = {.x=10, .y=-10};
+    point_t target = {.x=10, .y=0};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_BACK);
+    CHECK_EQUAL(elbow1.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowBackwardBis)
+{
+    point_t elbow1 = {.x=10, .y=-10};
+    point_t elbow2 = {.x=10, .y=10};
+    point_t target = {.x=10, .y=0};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_BACK);
+    CHECK_EQUAL(elbow2.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowForward)
+{
+    point_t elbow1 = {.x=10, .y=-10};
+    point_t elbow2 = {.x=10, .y=10};
+    point_t target = {.x=10, .y=0};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow1.y, chosen.y);
+}
+
+TEST(ChooseElbowPositionTestGroup, ChooseElbowForwardBis)
+{
+    point_t elbow1 = {.x=10, .y=10};
+    point_t elbow2 = {.x=10, .y=-10};
+    point_t target = {.x=10, .y=0};
+    point_t chosen;
+
+    chosen = choose_shoulder_solution(target, elbow1, elbow2, SHOULDER_FRONT);
+    CHECK_EQUAL(elbow2.y, chosen.y);
+}
---
+++ tests/arm_cinematics_get_angle.cpp
@@ -0,0 +1,35 @@
+#include "CppUTest/TestHarness.h"
+#include "math.h"
+
+extern "C" {
+#include "../arm_cinematics.h"
+}
+
+#define RAD(x) ((x/180.)*M_PI)
+
+TEST_GROUP(GetAngleFromArmPosTestGroup)
+{
+};
+
+TEST(GetAngleFromArmPosTestGroup, SimpleShoulderAngle)
+{
+    float angle;
+    point_t elbow, hand;
+    elbow.x = 100;
+    elbow.y = 0;
+    angle = compute_shoulder_angle(elbow, hand);
+
+    DOUBLES_EQUAL(0, angle, 1e-3);
+}
+
+TEST(GetAngleFromArmPosTestGroup, SimpleElbowAngle)
+{
+    float angle;
+    point_t elbow = {.x=10, .y=10};
+    point_t hand =  {.x=20, .y=20};
+
+    angle = compute_elbow_angle(elbow, hand);
+    DOUBLES_EQUAL(RAD(45), angle, 1e-3);
+}
+
+
